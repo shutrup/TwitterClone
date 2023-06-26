4ca180001c7d7c2a9256246b3c6594644c86640c
shutrup <magagaraev95@mail.ru>
2023-06-26 14:29:10 +0300

    checked

--- MyTwitter/ContentView.swift
+++ MyTwitter/ContentView.swift
@@ -13,6 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
+            
             Text("Hello, world!")
         }
         .padding()
