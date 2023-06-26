f351f079765b509bd9b8b663623cda3503d7e1ef
shutrup <magagaraev95@mail.ru>
2023-06-26 14:23:19 +0300

    Initial Commit

---
+++ MyTwitter/MyTwitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTwitterApp.swift
+//  MyTwitter
+//
+//  Created by Шарап Бамматов on 26.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct MyTwitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
