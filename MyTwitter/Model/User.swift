3decd9260aa78fd11f4b4f36a9025def9e6bdfbb
shutrup <magagaraev95@mail.ru>
2023-07-05 19:41:38 +0300

    Create user model and AuthVIewModel

---
+++ MyTwitter/Model/User.swift
@@ -0,0 +1,24 @@
+//
+//  User.swift
+//  MyTwitter
+//
+//  Created by Шарап Бамматов on 05.07.2023.
+//
+
+import Foundation
+
+struct User: Decodable, Identifiable {
+    var _id: String
+    var id: String {
+        return _id
+    }
+    let username: String
+    let name: String
+    let email: String
+    var location: String?
+    var bio: String?
+    var website: String?
+    var avatarExists: String?
+    var followers: [String]
+    var followings: [String]
+}
---
+++ MyTwitter/ViewModel/AuthViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  AuthViewModel.swift
+//  MyTwitter
+//
+//  Created by Шарап Бамматов on 05.07.2023.
+//
+
+import SwiftUI
+
+final class AuthViewModel: ObservableObject {
+    func login() {
+        
+    }
+    
+    func register() {
+        
+    }
+    
+    func logout() {
+        
+    }
+}
